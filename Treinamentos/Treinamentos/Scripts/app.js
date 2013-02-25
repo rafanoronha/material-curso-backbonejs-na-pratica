5447073fa2396e74a9e94d7024134bc003fc63e8
rafanoronha <rafanoronha@gmail.com>
2013-02-25 19:04:28 -0300

    Primeiros passos - solucao

--- Treinamentos/Treinamentos/Scripts/app.js
+++ Treinamentos/Treinamentos/Scripts/app.js
@@ -1,1 +1,26 @@
-﻿
+﻿var App = {};
+
+App.listarTurmas = function (opt) {
+    var el = new App.TurmasView().render().$el;
+    $('#page').empty().append(el);
+};
+
+App.Router = Backbone.Router.extend({
+    routes: {
+        '': 'home'
+    },
+    home: function () {
+        App.listarTurmas();
+    }
+});
+
+App.TurmasView = Backbone.View.extend({
+    template: _.template($('#turmasTp').html()),
+    render: function () {
+        this.$el.html(this.template());
+        return this;
+    }
+});
+
+App.router = new App.Router();
+Backbone.history.start();
