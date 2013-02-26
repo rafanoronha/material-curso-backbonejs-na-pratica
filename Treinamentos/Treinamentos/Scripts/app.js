4517676393ebf8a0187759645e1cd0852549dc3d
rafanoronha <rafanoronha@gmail.com>
2013-02-25 22:26:39 -0300

    Listar instrutores - solucao

--- Treinamentos/Treinamentos/Scripts/app.js
+++ Treinamentos/Treinamentos/Scripts/app.js
@@ -5,24 +5,44 @@ App.listarTurmas = function (opt) {
     $('#page').empty().append(el);
 };
 
+App.listarInstrutores = function () {
+    App.instrutores.fetch();
+
+    var el = new App.InstrutoresView({
+        collection: App.instrutores
+    }).render().$el;
+    
+    $('#page').empty().append(el);
+}
+
 App.Router = Backbone.Router.extend({
     routes: {
-        '': 'home'
+        '': 'home',
+        'instrutores' : 'listarInstrutores'
     },
     home: function () {
         App.listarTurmas();
+    },
+    listarInstrutores: function () {
+        App.listarInstrutores();
     }
 });
 
 App.MenuView = Backbone.View.extend({
     el: 'nav',
     events: {
-        'click [data-action=home]': 'home'
+        'click [data-action=home]': 'home',
+        'click [data-action=instrutores]': 'listarInstrutores'
     },
     home: function (e) {
         e.preventDefault();
         App.listarTurmas();
         App.router.navigate('');
+    },
+    listarInstrutores: function (e) {
+        e.preventDefault();
+        App.listarInstrutores();
+        App.router.navigate('instrutores');
     }
 });
 
@@ -34,6 +54,48 @@ App.TurmasView = Backbone.View.extend({
     }
 });
 
+App.Instrutor = Backbone.Model.extend({
+    urlRoot: '/api/instrutores/'
+});
+
+App.Instrutores = Backbone.Collection.extend({
+    url: '/api/instrutores/',
+    model: App.Instrutor
+});
+
+App.InstrutoresView = Backbone.View.extend({
+    template: _.template($('#instrutoresTp').html()),
+    initialize: function () {
+        this.collection.on('reset', this.renderData, this);
+    },
+    render: function () {
+        this.$el.html(this.template());
+        return this;
+    },
+    renderData: function () {
+        var tbody = this.$('tbody');
+        this.collection.each(function (instrutor) {
+            var view = new App.InstrutorView({
+                model: instrutor,
+            });
+            view.$el.appendTo(tbody);
+            view.render();
+        }, this);
+    }
+});
+
+App.InstrutorView = Backbone.View.extend({
+    tagName: 'tr',
+    template: _.template($('#instrutorTp').html()),
+    render: function () {
+        var content = this.template(this.model.toJSON());
+        this.$el.html(content);
+        return this;
+    }
+});
+
 App.router = new App.Router();
 App.menuView = new App.MenuView();
+App.instrutores = new App.Instrutores();
+
 Backbone.history.start();
