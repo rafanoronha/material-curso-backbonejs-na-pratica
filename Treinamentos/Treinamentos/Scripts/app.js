08e000caa5eec72de809df567c9de5d184078062
rafanoronha <rafanoronha@gmail.com>
2013-02-27 11:56:37 -0300

    Criar instrutor - Solucao

--- Treinamentos/Treinamentos/Scripts/app.js
+++ Treinamentos/Treinamentos/Scripts/app.js
@@ -15,17 +15,33 @@ App.listarInstrutores = function () {
     $('#page').empty().append(el);
 }
 
+App.formularioInstrutor = function (model) {
+    var el = new App.FormularioInstrutorView({
+        model: model
+    }).render().$el;
+
+    $('#page').empty().append(el);
+}
+
+App.novoInstrutor = function () {
+    App.formularioInstrutor(new App.Instrutor);
+};
+
 App.Router = Backbone.Router.extend({
     routes: {
         '': 'home',
-        'instrutores' : 'listarInstrutores'
+        'instrutores': 'listarInstrutores',
+        'instrutores/novo': 'novoInstrutor',
     },
     home: function () {
         App.listarTurmas();
     },
     listarInstrutores: function () {
         App.listarInstrutores();
-    }
+    },
+    novoInstrutor: function () {
+        App.novoInstrutor();
+    },
 });
 
 App.MenuView = Backbone.View.extend({
@@ -68,6 +84,9 @@ App.InstrutoresView = Backbone.View.extend({
     initialize: function () {
         this.collection.on('reset', this.renderData, this);
     },
+    events: {
+        'click [data-action=new]': 'new'
+    },
     render: function () {
         this.$el.html(this.template());
         return this;
@@ -81,6 +100,11 @@ App.InstrutoresView = Backbone.View.extend({
             view.$el.appendTo(tbody);
             view.render();
         }, this);
+    },
+    'new': function (e) {
+        e.preventDefault();
+        App.novoInstrutor();
+        App.router.navigate('instrutores/novo');
     }
 });
 
@@ -91,6 +115,32 @@ App.InstrutorView = Backbone.View.extend({
         var content = this.template(this.model.toJSON());
         this.$el.html(content);
         return this;
+    }
+});
+
+App.FormularioInstrutorView = Backbone.View.extend({
+    template: _.template($('#formularioInstrutorTp').html()),
+    events: {
+        'click [data-action=cancel]': 'cancel',
+        'click [data-action=submit]': 'submit'
+    },
+    render: function () {
+        this.$el.html(this.template());
+        return this;
+    },
+    submit: function () {
+        this.model.save({
+            nome: this.$('[name=nome]').val()
+        }, {
+            success: this.listarInstrutores
+        });
+    },
+    cancel: function () {
+        this.listarInstrutores();
+    },
+    listarInstrutores: function () {
+        App.listarInstrutores();
+        App.router.navigate('instrutores');
     }
 });
 
