47a96d77d8e32ed7e6a9b27be25ca652660b2f27
rafanoronha <rafanoronha@gmail.com>
2013-02-28 22:27:22 -0300

    Lidando com falhas no JavaScript - solucao

---
+++ Treinamentos/Treinamentos/Controllers/ErrorsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Treinamentos.Controllers
+{
+    public class ErrorsController : ApiController
+    {
+        public HttpResponseMessage PostError(Error error)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+
+    public class Error
+    {
+        public string Msg { get; set; }
+        public string Url { get; set; }
+        public int LineNumber { get; set; }
+    }
+}
